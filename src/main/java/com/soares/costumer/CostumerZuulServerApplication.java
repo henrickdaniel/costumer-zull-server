f7bd205cb6ab64f98ea6faaecc4db9f865e24d7b
Henrick Daniel Soares <henrickdaniel@gmail.com>
2018-12-24 10:35:36 -0200

    just beginning

---
+++ src/main/java/com/soares/costumer/CostumerZuulServerApplication.java
@@ -0,0 +1,12 @@
+package com.soares.costumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostumerZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostumerZuulServerApplication.class, args);
+	}
+}
