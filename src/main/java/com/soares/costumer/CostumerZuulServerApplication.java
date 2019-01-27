b56f1dfdcc6e45bf7870ccfef72093bd51f2f134
Henrick Daniel Soares <henrickdaniel@gmail.com>
2019-01-27 11:14:52 -0200

    correcting CostumerZuulServerApplication.java and bootstrap.yml

--- src/main/java/com/soares/costumer/CostumerZuulServerApplication.java
+++ src/main/java/com/soares/costumer/CostumerZuulServerApplication.java
@@ -1,9 +1,11 @@
 package com.soares.costumer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
+@EnableZuulProxy
+@SpringCloudApplication
 public class CostumerZuulServerApplication {
 
 	public static void main(String[] args) {
